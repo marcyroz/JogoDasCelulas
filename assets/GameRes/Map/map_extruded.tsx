31b6a6251eeaf54885016caa9c4ded8609528765
Marcy <marcelly.nascimento84@gmail.com>
2025-04-08 12:06:44 -0300

    feat: atualização da grid do tiledmap

--- assets/GameRes/Map/map_extruded.tsx
+++ assets/GameRes/Map/map_extruded.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.0" name="map_extruded" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+<tileset version="1.4" tiledversion="1.4.0" name="map_extruded" tilewidth="30" tileheight="30" tilecount="49" columns="7">
  <image source="map_extruded.png" width="226" height="226"/>
 </tileset>
